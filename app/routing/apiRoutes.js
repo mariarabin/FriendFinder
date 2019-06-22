48210b7a9dcab822483467ee613788e945bf393f
Maria <lrabin@gmail.com>
2019-06-21 23:04:26 -0400

    Added the post method

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,9 @@
 
 var friendsList = require("../data/friend");
+//var newFriend = "";
 
 
 module.exports = function (app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
 
     app.get("/routes/api/friends", function (req, res) {
         res.json(friendsList);
@@ -22,23 +18,23 @@ module.exports = function (app) {
     // Then the server saves the data to the tableData array)
     // ---------------------------------------------------------------------------
 
-    app.post("routes/api/friends", function (req, res) {
+    app.post("/routes/api/friends", function (req, res) {
         // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
         // It will do this by sending out the value "true" have a table
         // req.body is available since we're using the body parsing middleware
-        if (friendsList.length < 5) {
+        if (friendsList.length > 1) {
             friendsList.push(req.body);
             res.json(true);
+            console.log("Truelalu");
         }
         else {
-            waitListData.push(req.body);
+            friendsList.push(req.body);
             res.json(false);
+            console.log("Wrong");
         }
     });
 
-    // I added this below code so you could clear out the table while working with the functionality.
-
-
+    // Cear out the table while working with the functionality.
     app.post("/api/clear", function (req, res) {
         // Empty out the arrays of data
         friendsList.length = 0;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,8 +7,14 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 
-    // Survey page
+    // Survey Page
     app.get('/survey', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
+
+    // If no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
 };
