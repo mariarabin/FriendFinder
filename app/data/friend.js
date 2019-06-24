26fad80e1106c74bceb3a95cc3d1cf4c6251423d
Maria <lrabin@gmail.com>
2019-06-24 01:15:37 -0400

    Updated logic

--- app/data/friend.js
+++ app/data/friend.js
@@ -3,7 +3,7 @@
 var friendsList = [
     {
         name: 'Jean Grey',
-        photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjqzejyg_fiAhXNl-AKHVbIAZEQjhx6BAgBEAM&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ACute_dog.jpg&psig=AOvVaw3_Z-gbol8AHXrf3-vDzjF-&ust=1561084719465208',
+        photo: 'https://commons.wikimedia.org/wiki/File:Cute_dog.jpg',
         scores: [
             '3',
             '5',
@@ -18,7 +18,7 @@ var friendsList = [
     },
     {
         name: 'Scott Summers',
-        photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjqzejyg_fiAhXNl-AKHVbIAZEQjhx6BAgBEAM&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ACute_dog.jpg&psig=AOvVaw3_Z-gbol8AHXrf3-vDzjF-&ust=1561084719465208',
+        photo: 'https://commons.wikimedia.org/wiki/File:Cute_dog.jpg',
         scores: [
             '5',
             '4',
@@ -33,7 +33,7 @@ var friendsList = [
     },
     {
         name: 'Professor Charles Xavier',
-        photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjqzejyg_fiAhXNl-AKHVbIAZEQjhx6BAgBEAM&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ACute_dog.jpg&psig=AOvVaw3_Z-gbol8AHXrf3-vDzjF-&ust=1561084719465208',
+        photo: 'https://commons.wikimedia.org/wiki/File:Cute_dog.jpg',
         scores: [
             '5',
             '5',
@@ -48,7 +48,7 @@ var friendsList = [
     },
     {
         name: 'Storm',
-        photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjqzejyg_fiAhXNl-AKHVbIAZEQjhx6BAgBEAM&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ACute_dog.jpg&psig=AOvVaw3_Z-gbol8AHXrf3-vDzjF-&ust=1561084719465208',
+        photo: 'https://commons.wikimedia.org/wiki/File:Cute_dog.jpg',
         scores: [
             '2',
             '5',
@@ -62,19 +62,19 @@ var friendsList = [
             '1']
     },
     {
-        name: 'Gambit',
-        photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjqzejyg_fiAhXNl-AKHVbIAZEQjhx6BAgBEAM&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ACute_dog.jpg&psig=AOvVaw3_Z-gbol8AHXrf3-vDzjF-&ust=1561084719465208',
+        name: 'Jubilee',
+        photo: 'https://commons.wikimedia.org/wiki/File:Cute_dog.jpg',
         scores: [
-            '2',
-            '5',
-            '3',
-            '2',
-            '5',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
             '1',
             '2',
             '2',
-            '2',
-            '2']
+            '5']
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,38 +1,24 @@
 
 var friendsList = require("../data/friend");
 
-
-
 module.exports = function (app) {
 
     app.get("/routes/api/friends", function (req, res) {
         res.json(friendsList);
     });
 
-
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
-
+    // API POST Request
     app.post("/routes/api/friends", function (req, res) {
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
-        // req.body is available since we're using the body parsing middleware
         var newfriend = req.body;
         var userScore = newfriend.newQuestions;
 
-        //var total = 0;
         //Compute for Match
         var matchName = "";
         var matchPhoto = "";
         var matchdifference = 10000; // Make the initial value big for comparison
 
 
-        // Calculating totals 
+        // Calculating 
         for (var i = 0; i < friendsList.length; i++) {
             var diff = 0;
             for (var j = 0; j < userScore.length; j++) {
@@ -40,12 +26,13 @@ module.exports = function (app) {
                 //console.log(friendsList.scores.getOwnPropertyNames());
 
                 diff = Math.abs((userScore[j] - friendsList[i].scores[j]));
-                console.log("DIFF" + diff);
+                console.log("DIFF is" + diff);
 
                 if (diff <= matchdifference) {
                     matchName = friendsList[i].name,
                         matchPhoto = friendsList[i].photo,
                         matchdifference = diff
+                    console.log("MATCH DIFFERENCE is" + diff);
                 }
             }
         }
