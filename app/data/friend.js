29ebe7b571e704db1bb39bc9d77dea0bb473961b
Maria <lrabin@gmail.com>
2019-06-24 20:54:47 -0400

    Styling Updates

--- app/data/friend.js
+++ app/data/friend.js
@@ -3,22 +3,22 @@
 var friendsList = [
     {
         name: 'Jean Grey',
-        photo: 'https://commons.wikimedia.org/wiki/File:Cute_dog.jpg',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Cute_dog.jpg/800px-Cute_dog.jpg',
         scores: [
-            '3',
-            '5',
-            '5',
             '1',
-            '5',
             '3',
             '1',
             '1',
-            '3',
+            '1',
+            '1',
+            '5',
+            '2',
+            '2',
             '5']
     },
     {
         name: 'Scott Summers',
-        photo: 'https://commons.wikimedia.org/wiki/File:Cute_dog.jpg',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Cute_dog.jpg/800px-Cute_dog.jpg',
         scores: [
             '5',
             '4',
@@ -32,23 +32,8 @@ var friendsList = [
             '1']
     },
     {
-        name: 'Professor Charles Xavier',
-        photo: 'https://commons.wikimedia.org/wiki/File:Cute_dog.jpg',
-        scores: [
-            '5',
-            '5',
-            '3',
-            '1',
-            '5',
-            '3',
-            '2',
-            '1',
-            '3',
-            '1']
-    },
-    {
         name: 'Storm',
-        photo: 'https://commons.wikimedia.org/wiki/File:Cute_dog.jpg',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Cute_dog.jpg/800px-Cute_dog.jpg',
         scores: [
             '2',
             '5',
@@ -62,19 +47,19 @@ var friendsList = [
             '1']
     },
     {
-        name: 'Jubilee',
-        photo: 'https://commons.wikimedia.org/wiki/File:Cute_dog.jpg',
+        name: 'Professor Charles Xavier',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Cute_dog.jpg/800px-Cute_dog.jpg',
         scores: [
+            '5',
+            '5',
+            '3',
             '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
+            '5',
+            '3',
             '2',
-            '2',
-            '5']
+            '1',
+            '3',
+            '1']
     }
 ];
 
